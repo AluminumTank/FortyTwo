0748930c0fd24d13cf573431cca1e6207e54abde
Joel Beckmeyer <jbeckmeyer@runbox.com>
2017-12-08 14:47:48 -0500

    intial commit

---
+++ src/termproject/Termproject.java
@@ -0,0 +1,26 @@
+/*
+ * This class contains JUnit tests to run against the ArrayQueue class.
+@author Joel Beckmeyer
+@version 1.0
+File: Filename.java
+Created: YYYY-MM-DD
+Description: This class contains tests for correct exception handling in 
+several different cases, circular array wrapping, dynamic resizing, correct
+method returns, and correct (expected) queue behavior.
+ */
+package termproject;
+
+/**
+ *
+ * @author joel
+ */
+public class Termproject {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
